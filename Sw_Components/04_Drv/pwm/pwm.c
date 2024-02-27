a211f84ee8159274bd0f89302d988456bb15b463
inpgbburda <49471138+inpgbburda@users.noreply.github.com>
2024-02-27 20:26:34 +0100

    Refactor code and add comments

--- Sw_Components/04_Drv/pwm/pwm.c
+++ Sw_Components/04_Drv/pwm/pwm.c
@@ -18,7 +18,9 @@
     Macro definitions
 |===================================================================================================================================|
 */
-
+#define GPIO_AFRL 0x00U
+#define MODER0_ALT_FUN_MODE      GPIO_MODER_MODER0_1
+#define AFRL_TIM1TIM2_SEL        GPIO_AFRL_AFSEL0_0
 #define FIRST_PERIOD 0xFFU
 
 /*
@@ -50,23 +52,21 @@
 void PwmInit(void)
 {
     RCC->AHB1ENR = RCC_AHB1LPENR_GPIOALPEN;     /*Enable clock for GPIO*/
-    GPIOA->MODER |=  0x00000002U;
-    GPIOA->AFR[0] |= 0x00000001U; 
+    GPIOA->MODER |= MODER0_ALT_FUN_MODE;        /*Configure the desired I/O as an alternate function*/
+    GPIOA->AFR[GPIO_AFRL] |= AFRL_TIM1TIM2_SEL; /*Root desired TIMer as pin source*/
 
-    RCC->APB1ENR = RCC_APB1ENR_TIM2EN;          /*Enable clock for Timer*/
-    TIM2->ARR = FIRST_PERIOD;   /* Must be set before enabling automatic preload to avoid waiting for first overflow*/
-    TIM2->CCMR1 |= (TIM_CCMR1_OC1M_1|TIM_CCMR1_OC1M_2);         /*Pwm mode 1*/
-    TIM2->CCMR1 |=  TIM_CCMR1_OC1PE;                            /*Enable the Preload register*/
-    TIM2->CR1 |= TIM_CR1_ARPE;                                  /*Enable the auto-reload Preload register */
+    RCC->APB1ENR = RCC_APB1ENR_TIM2EN;  /*Enable clock for Timer*/
+    TIM2->ARR = FIRST_PERIOD;           /* Must be set before enabling automatic preload to avoid waiting for first overflow*/
+    TIM2->CCMR1 |= (TIM_CCMR1_OC1M_1|TIM_CCMR1_OC1M_2);     /*Pwm mode 1*/
+    TIM2->CCMR1 |=  TIM_CCMR1_OC1PE;                        /*Enable the Preload register*/
+    TIM2->CR1 |= TIM_CR1_ARPE;                              /*Enable the auto-reload Preload register */
+    TIM2->CCER |= TIM_CCER_CC1E;  /*Set OC1 signal is output on the corresponding output pin*/
 
     TIM2->ARR = PERIOD_VAL;
-    TIM2->CCR1 = DUTY_VAL;
-    
-    TIM2->CCER |= TIM_CCER_CC1E; 
-    TIM2-> CR1 |= TIM_CR1_CEN;                  /*Set Counter Enable Bit*/
+    TIM2->CCR1 = DUTY_VAL; 
 }
 
 void PwmStart()
 {
-
+    TIM2->CR1 |= TIM_CR1_CEN;  /*Set Counter Enable Bit*/
 }
