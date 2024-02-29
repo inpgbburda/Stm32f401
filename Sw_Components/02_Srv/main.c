0b19c3447bb10eafdaead7afde1ec6e84c5404be
inpgbburda <49471138+inpgbburda@users.noreply.github.com>
2024-02-29 06:59:23 +0100

    Add Spi template files
    - create header, config and soruce files and add them to Cmake
    - create emty init function and call it in main

--- Sw_Components/02_Srv/main.c
+++ Sw_Components/02_Srv/main.c
@@ -1,8 +1,11 @@
 #include "pwm.h"
+#include "spi.h"
 
 int main(void)
 {
     PwmInit();
+    SpiInit();
+
     PwmStart();
     
     while (1){
---
+++ Sw_Components/04_Drv/spi/spi_cfg.h
@@ -0,0 +1,8 @@
+/**
+* File contains 
+*
+*/
+#ifndef SPI_H_CFG
+#define SPI_H_CFG
+
+#endif /* SPI_H_CFG */
