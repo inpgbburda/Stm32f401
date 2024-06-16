ae4a664e797885565b363130501a039cf3ce843a
inpgbburda <49471138+inpgbburda@users.noreply.github.com>
2024-06-16 11:01:05 +0200

    Route spi pins into spi peripheral
    - tell ports to be configured as alternate function
    - connect to alternate function spi perihperal hardware

--- Sw_Components/04_Drv/spi/spi.c
+++ Sw_Components/04_Drv/spi/spi.c
@@ -17,6 +17,7 @@
 |===================================================================================================================================|
 */
 #define SPI_SR_RXNE_FLAG(reg) (reg & SPI_SR_RXNE_Msk)
+#define GPIO_AFRH 0x01U
 
 /*
 |===================================================================================================================================|
@@ -55,6 +56,18 @@ void SpiInit(const Spi_Cfg_T* config)
 {
     SPI_TypeDef* used_driver;
     RCC->APB1ENR |= RCC_APB1ENR_SPI2EN; /* Enable clock for SPI2*/
+
+    RCC->AHB1ENR |= RCC_AHB1LPENR_GPIOBLPEN; /*Enable clock for GPIO PORT B*/
+    /*Alternate options for SPI pins*/
+    GPIOB->MODER |= (GPIO_MODER_MODER15_1 | GPIO_MODER_MODER14_1 | GPIO_MODER_MODER13_1 | GPIO_MODER_MODER12_1); 
+    
+    /* Set up multiplexers for each SPI pin */
+    GPIOB->AFR[GPIO_AFRH] |=                        
+        GPIO_AFRH_AFSEL15_2 | GPIO_AFRH_AFSEL15_0 | /* AF5 for Pin15 */
+        GPIO_AFRH_AFSEL14_2 | GPIO_AFRH_AFSEL14_0 | /* AF5 for Pin14 */
+        GPIO_AFRH_AFSEL13_2 | GPIO_AFRH_AFSEL13_0 | /* AF5 for Pin13 */
+        GPIO_AFRH_AFSEL12_2 | GPIO_AFRH_AFSEL12_0;  /* AF5 for Pin12 */
+
     used_driver = config->instance;
 
     /* Clear register contents*/
