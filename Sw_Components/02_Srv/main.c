d25b18af6fb474d42f167dd214da68f2e7e4a28d
inpgbburda <bburdainpg@gmail.com>
2024-07-19 19:37:02 +0200

    Modify spi config file to comunicate with Nano board
    - Arduino Nano board is used for testing

--- Sw_Components/02_Srv/main.c
+++ Sw_Components/02_Srv/main.c
@@ -13,7 +13,7 @@ int main(void)
     PwmStart();
     
     while (1){
-        uint16_t spi_data = 0;
+        static uint16_t spi_data = 0;
         spi_data = SpiReadBuffer(SPI2);
     }
 }
--- Sw_Components/04_Drv/spi/spi_cfg.c
+++ Sw_Components/04_Drv/spi/spi_cfg.c
@@ -7,6 +7,9 @@
 #include "stm32f401xc.h"
 
 #define SPI_CR1_MSB_FIRST 0U
+#define SPI_CR1_DFF_8_BIT 0U
+#define SPI_CR1_CPOL_LOW_IDLE 0U
+#define SPI_CR1_CPHA_FIRST_EDGE 0U
 #define SPI_CR1_MSTR_SLAVE 0U
 #define SPI_CR2_MOT_MODE 0U
 #define SPI_CR1_SSM_HW 0U
@@ -15,13 +18,13 @@
 const Spi_Cfg_T Spi2_Config =
 {
     SPI2,
-    SPI_CR1_DFF,            /* 16 bit format */
-    SPI_CR1_CPOL,           /* high state as idle */
-    SPI_CR1_CPHA,           /* second edge latching */
+    SPI_CR1_DFF_8_BIT,       /* 8 bit format of data frame*/
+    SPI_CR1_CPOL_LOW_IDLE,   /* low sck state as idle */
+    SPI_CR1_CPHA_FIRST_EDGE, /* first edge of sck as latching */
     SPI_CR1_MSB_FIRST,
-    SPI_CR1_SSM_HW,         /* hw slave select*/
-    SPI_CR2_SSOE_DIS,       /* disable slave select output */
-    SPI_CR1_MSTR_SLAVE,     /* MOSI pin is a data input */
-    SPI_CR2_MOT_MODE,       /* motorola mode */
-    SPI_CR1_SPE             /* enable spi */
+    SPI_CR1_SSM_HW,          /* hw slave select*/
+    SPI_CR2_SSOE_DIS,        /* disable slave select output */
+    SPI_CR1_MSTR_SLAVE,      /* MOSI pin is a data input */
+    SPI_CR2_MOT_MODE,        /* motorola mode */
+    SPI_CR1_SPE              /* enable spi */
 };
