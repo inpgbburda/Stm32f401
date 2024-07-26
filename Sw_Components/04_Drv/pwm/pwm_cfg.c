23d53a4ddf9e4d96c4c0958fd27118a9ccc62097
inpgbburda <bburdainpg@gmail.com>
2024-07-26 14:27:30 +0200

    Change configuration to get 50Hz Pwm
    - note: measured freq was about 49.50hz, to be investigated later

--- Sw_Components/04_Drv/pwm/pwm_cfg.c
+++ Sw_Components/04_Drv/pwm/pwm_cfg.c
@@ -2,8 +2,8 @@
 
 const Pwm_Cfg_T Pwm2_Config = 
 {
-    TIM2,        /* instance */
-    1000U,       /* prescaler value*/
-    100U,        /* reload_val */
+    TIM2,      /* instance */
+    15U,       /* prescaler value*/
+    19999U,    /* reload_val: 16Mhz/16/20000= 50hz;  counter tic = 1us*/
     TIM_CCER_CC4E | TIM_CCER_CC3E | TIM_CCER_CC2E | TIM_CCER_CC1E /* Selected channels */
 };
