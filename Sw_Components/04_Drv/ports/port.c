781ecb440451850990e8f2ac3aad466a829f6762
inpgbburda <bburdainpg@gmail.com>
2024-07-30 20:14:17 +0200

    Prepare template for port config constnat

--- Sw_Components/04_Drv/ports/port.c
+++ Sw_Components/04_Drv/ports/port.c
@@ -3,19 +3,35 @@
 
 #define GPIO_AFRL 0x00U
 #define GPIO_AFRH 0x01U
-#define MODER0_ALT_FUN_MODE   GPIO_MODER_MODER0_1
 #define AFRL_TIM1TIM2_SEL     GPIO_AFRL_AFSEL0_0
+
+#define PORT_MAX_NUMBER 2
+
+typedef struct
+{
+    GPIO_TypeDef* port;
+    uint32_t moder;
+}
+Port_Cfg_T;
+
+const Port_Cfg_T Config[PORT_MAX_NUMBER] = {
+    {GPIOA,  GPIO_MODER_MODER0_1   },    /* Alternate function mode for TIM2 pins */
+    {GPIOB, (GPIO_MODER_MODER15_1 |      /* Alternate function mode for SPI2 pins */
+             GPIO_MODER_MODER14_1 | 
+             GPIO_MODER_MODER13_1 | 
+             GPIO_MODER_MODER12_1) }
+};
 
 void PortInit(void)
 {
-    /* Configure mode for PORT pins */
-    GPIOA->MODER |= MODER0_ALT_FUN_MODE;    /* Alternate function mode for TIM2 pins */
-    GPIOB->MODER |=                         /* Alternate function mode for SPI2 pins */
-        GPIO_MODER_MODER15_1 | 
-        GPIO_MODER_MODER14_1 | 
-        GPIO_MODER_MODER13_1 | 
-        GPIO_MODER_MODER12_1; 
-    
+    GPIO_TypeDef* port;
+
+    //iterate through all ports in config:
+    for(int i=0; i<PORT_MAX_NUMBER; i++)
+    {
+        port = Config[i].port;
+        port->MODER = Config[i].moder;
+    }
     /* Set up multiplexers for connecting PORTS with desired hw */
     GPIOA->AFR[GPIO_AFRL] |= AFRL_TIM1TIM2_SEL;     /* Root desired TIMer as pin source*/
     GPIOB->AFR[GPIO_AFRH] |=                        
