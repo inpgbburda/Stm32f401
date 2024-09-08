669a7ab603c8aca9f0cc03b9e198999141318fb6
inpgbburda <bburdainpg@gmail.com>
2024-09-08 16:45:46 +0200

    Fix max interrupt priority value and add documentation

--- Sw_Components/02_Srv/Os/Os_Api/os.c
+++ Sw_Components/02_Srv/Os/Os_Api/os.c
@@ -1,22 +1,56 @@
+/**
+ * @file os.c
+ * @brief This file contains the implementation of the operating system (OS) functions for a microcontroller using FreeRTOS.
+ *
+ */
+
 #include "os.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "spi.h"
 
+/**
+ * @brief This function is called if a task overflows its stack.
+ *
+ * This function is a FreeRTOS hook function that is called if a task's stack overflows.
+ * It is intended to provide debugging information about the task that caused the overflow.
+ *
+ * @param xTask The handle of the task that overflowed its stack.
+ * @param pcTaskName The name of the task that overflowed its stack.
+ *
+ * @return This function does not return. It enters an infinite loop to prevent the program from continuing.
+ */
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
     while(1){
 
     };
 }
 
-void vTask1(void *pvParameters) {
+/**
+ * @brief This function is a task that updates the PWM output.
+ * @param pvParameters A pointer to the task's parameters. This parameter is not used in this function.
+ *
+ * @return This function does not return. It runs in an infinite loop.
+ */
+void PwmTask(void *pvParameters) {
     for (;;) {
-        // Task 1 code
-        vTaskDelay(pdMS_TO_TICKS(1000));  // 1-second delay
+        /* Task updating the PWM output */
+        /* TODO */
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-void vTask2(void *pvParameters) {
+/**
+ * @brief This function is a task that reads data from an SPI peripheral and updates a variable.
+ *
+ * This function is a FreeRTOS task that continuously reads data from an SPI peripheral (SPI2 in this case)
+ * and updates a static variable 'spi_data'. It then delays for 10 milliseconds before repeating the process.
+ *
+ * @param pvParameters A pointer to the task's parameters. This parameter is not used in this function.
+ *
+ * @return This function runs in an infinite loop and does not return.
+ */
+void SpiTask(void *pvParameters) {
     for (;;) {
         static uint16_t spi_data = 0;
         spi_data = SpiReadBuffer(SPI2);
@@ -24,12 +58,24 @@ void vTask2(void *pvParameters) {
     }
 }
 
+/**
+ * @brief This function initializes the operating system (OS) by creating tasks.
+ *
+ * @return This function does not return.
+ */
 void OsInit(void)
 {
-    xTaskCreate(vTask1, "Task 1", configMINIMAL_STACK_SIZE + 100, NULL, 1, NULL);
-    xTaskCreate(vTask2, "Task 2", configMINIMAL_STACK_SIZE + 100, NULL, 2, NULL);
+    xTaskCreate(PwmTask, "PwmTask", configMINIMAL_STACK_SIZE + 100, NULL, 1, NULL);
+    xTaskCreate(SpiTask, "SpiTask", configMINIMAL_STACK_SIZE + 100, NULL, 2, NULL);
 }
 
+/**
+ * @brief This function starts the operating system scheduler.
+ *
+ * Starts the FreeRTOS scheduler, allowing the created tasks to run.
+ *
+ * @return This function does not return. It enters an infinite loop once the scheduler has started.
+ */
 void OsStart(void)
 {
     vTaskStartScheduler();
--- Sw_Components/02_Srv/Os/Os_Api/os.h
+++ Sw_Components/02_Srv/Os/Os_Api/os.h
@@ -1,2 +1,15 @@
+/**
+ * @file os.h
+ * @brief Header file for the operating system functions.
+ *
+ * This header file contains declarations for the operating system initialization and start functions.
+ */
+
+#ifndef OS_H
+#define OS_H
+
 void OsInit(void);
 void OsStart(void);
+
+
+#endif /* OS_H */
