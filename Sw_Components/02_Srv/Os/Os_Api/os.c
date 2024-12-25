a0303c26beddf7538a88b00fa78cc45ed9fa23e6
inpgbburda <bburdainpg@gmail.com.com>
2024-12-25 18:41:29 +0100

    Create a raw proto of qeueing infos between tasks

--- Sw_Components/02_Srv/Os/Os_Api/os.c
+++ Sw_Components/02_Srv/Os/Os_Api/os.c
@@ -7,8 +7,16 @@
 #include "os.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
+
 #include "spi.h"
 #include "spi_cfg.h"
+
+#define SPI_TASK_PERIOD_MS 10
+#define PWM_TASK_PERIOD_MS 20
+
+QueueHandle_t  qeue1;
+
 
 /**
  * @brief This function is called if a task overflows its stack.
@@ -34,10 +42,21 @@ void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
  * @return This function does not return. It runs in an infinite loop.
  */
 void PwmTask(void *pvParameters) {
+
+    uint8_t receivedValue[20];
+
     for (;;) {
         /* Task updating the PWM output */
         /* TODO */
-        vTaskDelay(pdMS_TO_TICKS(1000));
+    
+        if( qeue1 != NULL )
+        {
+            if( xQueueReceive( qeue1, &receivedValue, (TickType_t)10 ) == pdPASS )
+            {
+                /* xRxedStructure now contains a copy of xMessage. */
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(PWM_TASK_PERIOD_MS));
     }
 }
 
@@ -53,18 +72,42 @@ uint8_t buffer[4] = {0};
  *
  * @return This function runs in an infinite loop and does not return.
  */
+
+bool MessageReceived = true; 
+
 void SpiTask(void *pvParameters) {
+    /* Task reading data from an SPI peripheral */
+    uint8_t ulVar = 0;
+
     for (;;) {
-        SpiReadIt(&Spi_Storage, buffer, 4);
-        vTaskDelay(pdMS_TO_TICKS(120));
+        if( MessageReceived){
+            if( qeue1 != 0 )
+            {
+                /* Send an unsigned long. Wait for 10 ticks for space to become
+                available if necessary. */
+                if( xQueueSendToBack( qeue1,
+                                    ( void * ) &buffer[0],
+                                    ( TickType_t ) 10 ) != pdPASS )
+                {
+                    /* Failed to post the message, even after 10 ticks. */
+                }
+            }
+            for(int i =0; i<4; i++){
+                buffer[i] = 0;
+            }
+            MessageReceived = false;
+            SpiReadIt(&Spi_Storage, buffer, 4);
+        }
+        else{
+            /*Do nothing*/
+        }
+        vTaskDelay(pdMS_TO_TICKS(SPI_TASK_PERIOD_MS));
     }
 }
 
 void Spi2_RxCompleteCbk(void)
 {
-    for(int i =0; i<4; i++){
-        buffer[i] = 0;
-    }
+    MessageReceived = true;
 }
 
 /**
@@ -74,6 +117,7 @@ void Spi2_RxCompleteCbk(void)
  */
 void OsInit(void)
 {
+    qeue1 = xQueueCreate(10, 4);
     xTaskCreate(PwmTask, "PwmTask", configMINIMAL_STACK_SIZE + 100, NULL, 1, NULL);
     xTaskCreate(SpiTask, "SpiTask", configMINIMAL_STACK_SIZE + 100, NULL, 2, NULL);
 }
